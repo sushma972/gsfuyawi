d16ffe32f3540d059926a9e92ee16555109ef835
sushma972 <86293471+sushma972@users.noreply.github.com>
2021-06-30 17:44:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+
+var a=prompt('Enter the first variable:');
+
+function setup() {
+  createCanvas(400, 400);
+  var b2=createButton("click here to swap");
+}
+
+function draw() {
+  background(220);
+}
